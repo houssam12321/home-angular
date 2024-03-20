40bd137e6b04626b0feb5ea0f609688901f4f7af
houssam radi <houssamradi48@gmail.com>
2024-03-20 10:52:38 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [HomeComponent],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" width="150rem" src="/assets/Design sans titre (2).png" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+  
+`,
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'home-angular';
 }
+imports: [
+  CommonModule,
+  HomeComponent,
+]
